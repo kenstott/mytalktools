d57d23d6889cec9b524ec2628c024db937dfa8e4
Kenneth Stott <ken@kenstott.com>
2022-12-30 18:30:46 -0500

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Kenneth Stott on 12/30/22.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
